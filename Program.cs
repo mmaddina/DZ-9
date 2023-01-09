5d1d7d5c864d2750b2c6d76acb49052cd28f0333
mmaddina <mamedovamadinaa8@gmail.com>
2023-01-09 21:25:28 +0300

    Task 66 done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 //N = 5 -> "5, 4, 3, 2, 1"
 //N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
+/*
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
@@ -19,11 +19,39 @@ void NaturalNum(int n, int count)
         Console.Write(count + " ");
     }
 }
+*/
 
 //Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
+ /*
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+*/
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
