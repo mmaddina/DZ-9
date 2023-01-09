dc8c929ff6ec2f5ea2c35610fd9ef8f16137c482
mmaddina <mamedovamadinaa8@gmail.com>
2023-01-09 21:28:29 +0300

    Task 68 done

--- Program.cs
+++ Program.cs
@@ -55,3 +55,33 @@ int SumMN(int m, int n)
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
+/*
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(AkkermanFunc(m, n)); 
+}
+
+int AkkermanFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkermanFunc(m - 1, 1);
+    }
+    else
+    {
+        return (AkkermanFunc(m - 1, AkkermanFunc(m, n - 1)));
+    }
+}
+*/
